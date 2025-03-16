fae9286ffe7d700b563b084c2466695740487fd1
Javed Akhtar <jved004@gmail.com>
2025-03-16 17:27:13 +0530

    fixed some file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,8 @@
-import { Route, Routes } from "react-router"
-import Home from "./components/Home.jsx"
-import TermsAndConditions from "./pages/TermsAndConditions.jsx"
-import PrivacyPolicy from "./pages/PrivacyPolicy.jsx"
-import UpcomingEvents from "./components/UpcomingEvents.jsx"
-import CreateEvent from "./components/CreateEvent.jsx"
-import EventsProvider from "./context/EventsProvider.jsx"
-
+import React from 'react'
+import Communion from './complete_app/Communion'
 
 function App() {
-
-  return (
-    <>
-    <EventsProvider>
-    <Routes>
-    <Route path="/" element={<Home />}/>
-    <Route path="/termsandconditions" element={<TermsAndConditions />}/>
-    <Route path="/privacypolicy" element={<PrivacyPolicy />}/>
-    <Route path="/upcoming-events" element={<UpcomingEvents />}/>
-    <Route path="/create-event" element={<CreateEvent />}/>
-    </Routes>
-    </EventsProvider>
-    </>
-  )
+  return <Communion />
 }
 
 export default App
